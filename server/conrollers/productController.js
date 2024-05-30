323f6703bd542141b028a1abdea7b4dba4726958
Ramprasadnemala <Ramprasadnemala12@gmail.com>
2024-05-30 17:08:01 +0530

    all changes2258

--- server/conrollers/productController.js
+++ server/conrollers/productController.js
@@ -1,9 +1,8 @@
 const Product = require('../models/Product');
 
-// Create a new product
-const createProduct = async (req, res) => {
-    try {
-        const { name, userNo, earn, expense, date, work } = req.body;
+const createProduct= async(req, res)=>{
+    try{
+        const {name,userNo,earn,expense,date,work}= req.body
 
         const product = new Product({
             name,
@@ -12,28 +11,23 @@ const createProduct = async (req, res) => {
             expense,
             date,
             work
-        });
+        })
 
-        await product.save();
-        res.status(201).json(product);
-    } catch (error) {
-        console.log("There is an error", error);
-        res.status(500).json({ message: "Server error" });
+        await product.save()
+        res.status(201).json(product)
+    }catch (error){
+        console.log("there is an error",error)
     }
-};
+}
 
-// Get all products with a 60-second delay
-const getProducts = async (req, res) => {
-    try {
-        // Delay for 60 seconds (60000 milliseconds)
-        await new Promise(resolve => setTimeout(resolve, 40000));
-
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        console.log("There is an error:", error);
-        res.status(500).json({ message: "Server error" });
+const getProducts=async(req,res)=>{
+    try{
+        const products =await Product.find()
+        res.status(200).json(products)
+    } catch(error){
+        console.log("There is an error:",error)
+        res.status(500).json({message:"server error"})
     }
-};
+}
 
-module.exports = { createProduct, getProducts };
+module.exports ={createProduct, getProducts }

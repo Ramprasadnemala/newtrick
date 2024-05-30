064d1dfde7f35531e0ee81c98be8cbe5f3c252dd
Ramprasadnemala <Ramprasadnemala12@gmail.com>
2024-05-30 11:22:39 +0530

    npm change22s

--- server/index.js
+++ server/index.js
@@ -1,39 +1,24 @@
 const express = require('express');
-const dotEnv = require('dotenv');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const employeeRoutes = require('./routes/employeeRoutes');
-const cors = require('cors');
-
 const app = express();
+require('dotenv').config();
+// require('./db');
+const PORT = process.env.PORT || 8080;
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+app.use(express.json());
 
-// Load environment variables from .env file
-dotEnv.config();
-
-// Define the port from the environment variables or default to 5000
-const PORT = process.env.PORT || 5000;
-
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-app.use(express.json())
-// Enable CORS for all routes
-app.use(cors({
-    origin: '*'
-}))
-
-// Connect to MongoDB using the connection string from .env
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("MongoDB Connected Successfully");
-    })
-    .catch((error) => {
-        console.log("Error", error);
-    });
-
-// Use the employee routes for any endpoint starting with /employees
-app.use('/employees', employeeRoutes);
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
+app.get('/', (req, res) => {
+    res.send('products api running new deploy');
 });
+
+app.get('/ping', (req, res) => {
+    res.send('PONG')
+});
+// /products
+app.use('/products', productRoutes);
+// /users
+app.use('/users', userRoutes);
+
+app.listen(8080, () => {
+    console.log('Server is listenin on PORT :' + PORT);
+})

0d11b8c9316fb9f20c934bd7bc55862b03e375b9
Ramprasadnemala <Ramprasadnemala12@gmail.com>
2024-05-30 13:22:49 +0530

    path changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
+
 // require('./db');
 const PORT = process.env.PORT || 8080;
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
+
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 app.get('/', (req, res) => {
     res.send('products api running new deploy');
